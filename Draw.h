2ec9a9ab369416b1216196b1cf3fbd5f40ba0ece
DogeSSBM <dogessbm@gmail.com>
2019-08-24 03:07:31 -0400

    Walls

--- Draw.h
+++ Draw.h
@@ -1,27 +1,52 @@
 #pragma once
 
-typedef enum{DIR_U, DIR_D, DIR_L, DIR_R}Dir;
+typedef enum{DIR_R, DIR_D, DIR_L, DIR_U}Dir;
+typedef enum{C_INNER, C_OUTER}CornerType;
 
 typedef struct{
-
+	int xoffset, yoffset;
+	Img* sprite;
 }Wall;
 
-void drawStraightWall(int x, int y, Dir offset)
+Wall wall[4];
+Wall wallDouble[4];
+Wall cornerInner[4];
+Wall cornerInnerDouble[4];
+Wall cornerOuter[4];
+Wall cornerOuterDouble[4];
+
+Img *loadWalls(void){
+	wall[0].sprite  = loadImg("./assets/wVs.png");
+	wall[0].xoffset =  SCALE;
+	wall[0].yoffset =  0;
+	wallDouble[0].sprite  = loadImg("./assets/wVd.png");
+	wallDouble[0].xoffset =  SCALE;
+	wallDouble[0].yoffset =  0;
+
+	wall[1].sprite  = loadImg("./assets/wHs.png");
+	wall[1].xoffset =  0;
+	wall[1].yoffset =  SCALE;
+	wallDouble[1].sprite  = loadImg("./assets/wHd.png");
+	wallDouble[1].xoffset =  0;
+	wallDouble[1].yoffset =  SCALE;
+
+	wall[2].sprite  = loadImg("./assets/wVs.png");
+	wall[2].xoffset = -SCALE;
+	wall[2].yoffset =  0;
+	wallDouble[2].sprite  = loadImg("./assets/wVd.png");
+	wallDouble[2].xoffset = -SCALE;
+	wallDouble[2].yoffset =  0;
+
+	wall[3].sprite  = loadImg("./assets/wHs.png");
+	wall[3].xoffset =  0;
+	wall[3].yoffset = -SCALE;
+	wallDouble[3].sprite  = loadImg("./assets/wHd.png");
+	wallDouble[3].xoffset =  0;
+	wallDouble[3].yoffset = -SCALE;
+}
+
+void drawWall(int x, int y, Dir offset)
 {
-
-	switch (offset) {
-	case DIR_U:
-		y-=4;
-		break;
-	case DIR_D:
-		y+=4;
-		break;
-	case DIR_L:
-		x-=4;
-		break;
-	case DIR_R:
-		x+=4;
-		break;
-	}
-
+	setColor(BLUE);
+	drawImg(wall[offset].sprite, (x+wall[offset].xoffset)*SCALE, (y+wall[offset].yoffset)*SCALE, SCALE, SCALE);
 }
--- main.c
+++ main.c
@@ -1,27 +1,35 @@
+#define XLEN 28
+#define YLEN 31
+#define SCALE 64
 #include "Includes.h"
 
 int main(int argc, char const *argv[])
 {
-	init_all(1200, 600);
-	fillScreen();
-	const char* sprites[] = {
-		"assets/doge0r.png",	"assets/doge0l.png",
-		"assets/doge0u.png",	"assets/doge0d.png",
-		"assets/doge1r.png",	"assets/doge1l.png",
-		"assets/doge1u.png",	"assets/doge1d.png",
-		"assets/dot0.png",	"assets/dot1.png",
-		"assets/wHs.png",		"assets/wVs.png",
-		"assets/wHdu.png",	"assets/wHdd.png",
-		"assets/wVdl.png",	"assets/wVdr.png",
-		"assets/wLs0.png",	"assets/wLs1.png",
-		"assets/wLs2.png",	"assets/wLs3.png",
-		"assets/wLd0.png",	"assets/wLd1.png",
-		"assets/wLd2.png",	"assets/wLd3.png",
-	};
-	for(uint i = 0; i < 12; i++){
-		loadDrawImg(sprites[i], 64*i+i*16, 0, 64, 64);
-		loadDrawImg(sprites[i+12], 64*i+i*16, 64+16, 64, 64);
-	}
+	init_all(XLEN*SCALE, YLEN*SCALE);
+	// const char* sprites[] = {
+	// 	"assets/doge0r.png",	"assets/doge0l.png",
+	// 	"assets/doge0u.png",	"assets/doge0d.png",
+	// 	"assets/doge1r.png",	"assets/doge1l.png",
+	// 	"assets/doge1u.png",	"assets/doge1d.png",
+	// 	"assets/dot0.png",	"assets/dot1.png",
+	// 	"assets/wHs.png",		"assets/wVs.png",
+	// 	"assets/wHdu.png",	"assets/wHdd.png",
+	// 	"assets/wVdl.png",	"assets/wVdr.png",
+	// 	"assets/wLs0.png",	"assets/wLs1.png",
+	// 	"assets/wLs2.png",	"assets/wLs3.png",
+	// 	"assets/wLd0.png",	"assets/wLd1.png",
+	// 	"assets/wLd2.png",	"assets/wLd3.png",
+	// };
+	// for(uint i = 0; i < 12; i++){
+	// 	loadDrawImg(sprites[i], 64*i+i*16, 0, 64, 64);
+	// 	loadDrawImg(sprites[i+12], 64*i+i*16, 64+16, 64, 64);
+	// }
+	loadWalls();
+	drawWall(4,4,DIR_R);
+	drawWall(4,5,DIR_L);
+
+	drawWall(8,4,DIR_D);
+	drawWall(9,4,DIR_U);
 	draw();
 	while(1){
 		events();
